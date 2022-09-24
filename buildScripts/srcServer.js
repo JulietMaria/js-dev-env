0eb316d33754fde5686d0a454a464b06205ed01b
JulietMaria <JulietMaria@github.com>
2022-09-24 12:05:51 +0530

    updated srcServer

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,6 +4,8 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+/* eslint-disable no-console */
+
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
@@ -16,7 +18,6 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
-
 
 app.get('/users', function(req, res) {
     // Hard coding for simplicity. Pretend this hits a real database
